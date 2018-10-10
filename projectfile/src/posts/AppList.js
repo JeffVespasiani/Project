16977acf62b95713b2889e7158c7ab8e8e17d873
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-10 10:04:34 -0400

    Update projectfile/src/posts/AppList.js

--- projectfile/src/posts/AppList.js
+++ projectfile/src/posts/AppList.js
@@ -20,7 +20,7 @@ class App extends Component {
 	render() {
 		var MyList = this.state.data.map(function(myObj,index){
 			return <ul>
-				<li key={ index }>{myObj['im:name'].label}</li>
+				<li key={ index }>{myObj['im:name'].label}<img src={myObj['im:image'][0].label} /></li>
 			</ul>
 		})
 		return <div>{ MyList }</div>
