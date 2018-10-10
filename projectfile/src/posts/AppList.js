0b5319c575f277ecacc2ff4decfa698dc32c332a
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-10 10:01:46 -0400

    Update projectfile/src/posts/AppList.js

--- projectfile/src/posts/AppList.js
+++ projectfile/src/posts/AppList.js
@@ -3,33 +3,39 @@ import AppData from '../data/apple.json'
 
 class App extends Component {
 	
-  constructor(props) {
-		super(props);
+  constructor() {
+		super();
 		this.state = { data: [], };
 	}
 	
 	componentDidMount() {
-		return (fetch('http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json')
-		.then((Response) => Response.json()))
+		fetch('http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json')
+		.then((Response) => Response.json())
 		.then((findresponse) => {
 			console.log(findresponse.feed)
-			this.setState({ data : findresponse.feed })
+			this.setState({ data : findresponse.feed.entry })
 		})
 	}
 	
 	render() {
-		console.log(this.state.data);
+		var MyList = this.state.data.map(function(myObj,index){
+			return <ul>
+				<li key={ index }>{myObj['im:name'].label}</li>
+			</ul>
+		})
+		return <div>{ MyList }</div>
+		/*console.log(this.state.data);
 		return (
 			<div>
 			{
 				Object.keys(this.state.data).map((listData, key) => 
 				<ul>
-					<li><p>{listData.entry.entry["im:name"].label}</p><img src={listData.entry.entry["im:images"].label[0]} /></li>
+					<li><p>{listData.entry.entry["im:name"].label}</p><img src={listData.entry.entry["im:image"].label[0]} /></li>
 				</ul>
 				)
 			}
 			</div>
-		);
+		);*/
 	/*render() {
     return (
 		<div>
