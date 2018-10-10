405bb99d04f6a43fd8ddca67b3addd3e25743e9c
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-10 03:15:21 -0400

    Update projectfile/src/App.js

--- projectfile/src/App.js
+++ projectfile/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AppList from './posts/AppList'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+		<AppList />
+		/*<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +21,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header>*/
       </div>
     );
   }
