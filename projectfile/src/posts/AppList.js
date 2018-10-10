7365ca5cc89538b08949fe44667afac47577dcc8
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-10 17:03:35 -0400

    Update projectfile/src/posts/AppList.js

--- projectfile/src/posts/AppList.js
+++ projectfile/src/posts/AppList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../css/style.css';
 
 class App extends Component {
 	
@@ -18,9 +19,20 @@ class App extends Component {
 	
 	render() {
 		var MyList = this.state.data.map(function(myObj,index){
-			return <ul>
-				<li key={ index }><img src={myObj['im:image'][0].label} /><br />{myObj['im:name'].label}</li>
-			</ul>
+			return [
+				<div key={1}><button className="menu" data-toggle="collapse"><img src={myObj['im:image'][0].label} /><br />{myObj['im:name'].label}</button></div>,
+				<div key={2}><ul>
+					<li>title: {myObj['im:name'].label}</li>
+					<li>icon: <img src={myObj['im:image'][1].label} /></li>
+					<li>release date: {myObj['im:releaseDate'].label}</li>
+					<li>summary: {myObj.summary.label}</li>
+					<li>price: {myObj['im:price'].label} {myObj['im:price'].attributes.currency}</li>
+					<li>category: {myObj.category.attributes.label}</li>
+					<li>linktostore: {myObj.link.attributes.href}</li>
+					<li>publisher: {myObj['im:artist'].label}</li>
+					<li>publisherlink: {myObj['im:artist'].attributes.href}</li>
+				</ul></div>
+			]
 		})
 		return <div>{ MyList }</div>
   }
