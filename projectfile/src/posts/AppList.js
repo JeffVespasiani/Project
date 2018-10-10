d4fc1079b8a59f68dc27b6ead6ea7e28f80c3de4
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-10 03:15:26 -0400

    Update projectfile/src/posts/AppList.js

---
+++ projectfile/src/posts/AppList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+//import AppData from '..data/apple.json'
+
+class App extends Component {
+  constructor(props) {
+		super(props);
+		this.state = { data: [], };
+	}
+	
+	componentDidMount() {
+		return (fetch('http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json')
+		.then((Response) => Response.json()))
+		.then((findresponse) => {
+			console.log(findresponse.feed)
+			this.setState({ data : findresponse.feed })
+		})
+	}
+	
+	render() {
+		console.log(this.state.data);
+		return (
+			<div>
+			{
+				Object.keys(this.state.data).map((dynamicData, key) => 
+				<ul>
+					<li><p>{dynamicData.entry.entry["im:name"].label}</p></li>
+				</ul>
+				)
+			}
+			</div>
+		);
+	}/*render() {
+    return (
+		<div>
+			<h1>Test</h1>
+			{AppData.feed.entry.map((appDetail, index)=>{
+				return <p>{appDetail.label}</p>
+			})}
+		</div>
+    );
+  }*/
+}
+
+export default App;
