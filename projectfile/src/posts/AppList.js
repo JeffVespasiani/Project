53b0b5b442027006f1c0056ee9c86775fb68b40d
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-11 00:16:49 -0400

    Update projectfile/src/posts/AppList.js

--- projectfile/src/posts/AppList.js
+++ projectfile/src/posts/AppList.js
@@ -37,14 +37,14 @@ class App extends Component {
 			return [
 			<div key={1}><h1>Welcome to the app store list!</h1><p>Click a title to learn more about it.</p></div>,
 			<div key={2}> {this.state.data.map((myObj,index) =>
-				<button className="menu" onClick={this.toggle}><img src={myObj['im:image'][0].label} /><br />{myObj['im:name'].label}<ul className="list" style={show}>
+				<div id={index}><button className="menu" onClick={this.toggle.bind(this, index)}><img src={myObj['im:image'][0].label} /><br />{myObj['im:name'].label}<ul className="list" style={show}>
 					<li>Release Date: {moment(myObj['im:releaseDate'].label).format('MM/DD/YYYY')}<br /><br /></li>
 					<li>{myObj.summary.label}<br /><br /></li>
 					<li>Price: {myObj['im:price'].label} {myObj['im:price'].attributes.currency}<br /><br /></li>
 					<li>Category: {myObj.category.attributes.label}<br /><br /></li>
 					<li><a href={myObj.link.attributes.href}>Buy it here!</a><br /><br /></li>
 					<li>Publisher: <a href={myObj['im:artist'].attributes.href}>{myObj['im:artist'].label}</a><br /><br /></li>
-				</ul></button>,
+				</ul></button></div>,
 				
 				
 			)}
